13eb7ed0f88b1fd0b07ff02b840fa59aff8fe88f
shir3bar <delusionalmind@gmail.com>
2022-07-07 15:57:27 +0300

    created new gui for the RDag project

--- RDagLabelerGUI.py
+++ RDagLabelerGUI.py
@@ -9,8 +9,8 @@ import cv2
 import numpy as np
 
 class RDagLabeler(Labeler):
-    KEYS_TO_LABELS = {'1': 'swim','2': 'strike','3':'Spit/I&O','3': 'Other'}
-    LABEL_LIST = ['swim','strike','Spit/I&O','Other']
+    KEYS_TO_LABELS = {'1': 'Swim','2': 'Strike','3':'Spit/I&O','3': 'Other'}
+    LABEL_LIST = ['Swim','Strike','Spit/I&O','Other']
     LABEL_MULTICHOICE = False
 
    # def __init__(self):
@@ -21,7 +21,7 @@ class RDagLabeler(Labeler):
         # If a video file is currently open, release it:
         if self.player:
             if self.player.curr_vid:
-                self.player.curr_vid.release()
+                self.player.curr_vid.stop()#.release()
                 if not self.log_saved:
                     # If the log has been change without saving, open a window prompt:
                     result = messagebox.askquestion('Save log','Do you want to save changes?')
@@ -45,40 +45,58 @@ class RDagLabeler(Labeler):
         # Define the movie player that will handle video display and navigation,
         # we pass the label var and comment widget so that their values can be set when we load a new
         # video to the GUI:
-        self.player = UncuratedMoviePlayer(self.window, label_var=self.label, comment_widget=self.ent_comment,
+        self.player = RDagMoviePlayer(self.window, label_var=self.label, comment_widget=self.ent_comment,
                                   multichoice=self.LABEL_MULTICHOICE)
         self.player.load_directory()  # See the Movie Player class for details
         self.bind_keystrokes()
 
 
-class UncuratedMoviePlayer(MoviePlayer):
+class RDagMoviePlayer(MoviePlayer):
 
-    LOG_FILENAME = 'labeled_preds.csv'
+    LOG_FILENAME = 'preds_labeled.csv'
     FOLDERNAME_TO_IGNORE = 'removed_doubles'
     COORDINATE_COLUMN_NAME = 'centroid'
-    #def __init__(self,window, label_var=[],comment_widget=[], multichoice=False):
-    #    super().__init__(window,label_var,comment_widget,multichoice)
+
 
     def handle_missing_log(self):
         """ Override the behavior of MoviePlayer for handling missing log files"""
-        msg = ''
-        for filename in ['preds_without_removed.csv','preds.csv']:
-            if os.path.isfile(os.path.join(self.directory,filename)):
-                self.log_filepath = os.path.join(self.directory, 'preds.csv')  # save path
-                self.log = pd.read_csv(self.log_filepath)  # load log
-                if self.multichoice:
-                    for key in self.label_var.keys():
-                        if self.column_names[key] not in self.log.columns:
-                            self.log[self.column_names[key]] = 0
-                else:
-                    self.log['reviewer_label'] = np.NaN
-                self.log.to_csv(self.log_filepath,index=False)
-                break
-        if len(msg)==0:
-            msg = f'Error no preds file found. App will not function right, choose a different folder.'
-        messagebox.showinfo('Notice', msg)
+
+        filename = 'preds.csv'
+        if os.path.isfile(os.path.join(self.directory,filename)):
+            self.log_filepath = os.path.join(self.directory, 'preds.csv')  # save path
+            self.log = pd.read_csv(self.log_filepath)  # load log
+            if 'clip_name' in self.log.columns:
+                # if log uses old column names, change it to comply with new naming system
+                self.log = self.log.loc[self.log.clip_name.notna(),
+                           :]  # filter out swims (which don't have a clip name) leave only strikes
+        else:
+            self.log = pd.DataFrame(columns=['vid_name','frame','clip_name','fish_id','centroid',
+                                             'bboxs','detection_scores','detection_pred_class','action_preds',
+                                             'strike_scores','reviewer_label'])
+            for i, vid in enumerate(self.file_paths):
+                # Iterate over the video files that were loaded and enter them as new rows in the dataframe:
+                clip_name = os.path.basename(vid)  # Get video name
+                entry = self.get_entry(clip_name)
+                self.log.loc[i, :] = entry
+        self.log['reviewer_label'] = np.NaN
+        new_filename = filename.split('.')[0] + '_labeled.csv'
+        self.log_filepath = os.path.join(self.directory, new_filename)
+        self.log.to_csv(self.log_filepath,index=False)
 
 
+    def load_log(self,root,filename):
+        self.log_filepath = os.path.join(root, filename)  # save path
+        self.log = pd.read_csv(self.log_filepath)  # load log
+        if not self.multichoice:
+            if 'label' in self.log.columns:
+                # if log uses old column names, change it to comply with new naming system
+                self.log = self.log.rename(columns={'label': 'reviewer_label'})
+            elif not 'reviewer_label' in self.log.columns:
+                self.log['reviewer_label'] = np.NaN
+        else:
+            for key in self.label_var.keys():
+                if self.column_names[key] not in self.log.columns:
+                    self.log[self.column_names[key]] = 0
 
     def get_entry(self,clip_name):
         # name format is [experiment_name]_midframe_[frame_num]_fish_[fish_id]_coordinate_[centroidx-centroidy].avi
@@ -86,8 +104,11 @@ class UncuratedMoviePlayer(MoviePlayer):
         frame_num = int(processed_name[-5])
         fish_id = int(processed_name[-3])
         coords = processed_name[-1].split('-')
+        vid_name = processed_name[0]
         # As we don't have any of the data about the parent video, we'll leave it blank for the user to fill later:
-        entry = {'frame': frame_num,
+        entry = {'vid_name':vid_name,
+        'frame': frame_num,
+        'clip_name': clip_name,
         'fish_id': fish_id,
         'centroid': coords,
         'bboxs': np.NaN,
@@ -95,11 +116,11 @@ class UncuratedMoviePlayer(MoviePlayer):
         'detection_pred_class': np.NaN,
         'action_preds': np.NaN,
         'strike_scores': np.NaN,
-        'strike_labels': np.NaN,
-        'spit_labels': np.NaN,
-        'clip_name': clip_name,
         'reviewer_label':None}
         return entry
+
+
+
 
 
 if __name__ == '__main__':
